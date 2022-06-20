f7837282fd2c732bcdbf29d235fe8e99cab75e13
Balla <ballaphanindra@outlook.com>
2022-06-20 22:22:00 +0530

    updating Program.cs

--- Day8Assignment/Day8Assignment/Program.cs
+++ Day8Assignment/Day8Assignment/Program.cs
@@ -1,31 +1,70 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Day8Assignment
 {
-    internal class Program
+    internal class EmployeeAttendance
     {
-        static void Main(string[] args)
+        static int WagePerHour = 20;
+        static int MaxNoOfWorkingDays = 20;
+        static int MaxNumberOfHours = 100;
+        static int WorkingHours = 0;
+        static int WorkingDays = 0;
+        static int TotalSalary = 0;
+
+        public void getAttendance()
+
         {
-            Console.WriteLine("Welcome to Employee Wage Computation program on master branch");
-            Console.WriteLine("========UC1=======");
-            UC1 u1 = new UC1();
-            u1.EmpCheck();
-            Console.WriteLine("========UC2=======");
-            UC2 u2 = new UC2();
-            u2.EmpWage();
-            Console.WriteLine("========UC3=======");
-            UC3 uc3 = new UC3();
-            int x = uc3.PartTimeWage();
-            Console.WriteLine("Part time wage of an employee is " + x);
-            Console.WriteLine("========UC4=======");
-            UC4 uc4 = new UC4();
-            uc4.SwitchUsage();
-            Console.WriteLine("========UC5=======");
-            UC5 u5 = new UC5();
-            u5.WagePerMonth();
-            Console.WriteLine("========UC6=======");
-            UC6 u6 = new UC6();
-            u6.WagePerMonthOrDays();
+
+            Random random = new Random();
+            while (WorkingDays <= MaxNoOfWorkingDays && WorkingHours <= MaxNumberOfHours)
+            {
+                int number = random.Next(0, 3);
+                CheckAttendance(number);
+            }
         }
+
+
+        public static void CheckAttendance(int number)
+        {
+            int hours;
+            switch (number)
+            {
+                case 0:
+
+                    Console.WriteLine("employee is absent");
+                    hours = 0;
+                    CalculateSalary(hours);
+                    break;
+
+                case 1:
+                    Console.WriteLine("Employee is present part time");
+                    hours = 4;
+                    WorkingDays++;
+                    WorkingHours = WorkingHours + hours;
+                    CalculateSalary(hours);
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("Employee is present full time");
+                    hours = 8;
+                    WorkingDays++;
+                    WorkingHours = WorkingHours + hours;
+                    CalculateSalary(hours);
+                    break;
+
+            }
+        }
+
+        public static void CalculateSalary(int hours)
+        {
+            int salary = WagePerHour * hours;
+            Console.WriteLine("Salary is " + salary);
+        }
+
     }
 }
